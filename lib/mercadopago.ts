80a666a9108974dbbda2b3ff0636647cf3d11acc
barbademoi <barbeariademoi@gmail.com>
2026-07-18 10:46:03 -0300

    Remove obsolete Mercado Pago file lib/mercadopago.ts

--- lib/mercadopago.ts
+++
@@ -1,8 +0,0 @@
-import MercadoPagoConfig, { Payment, Preference } from 'mercadopago'
-
-const client = new MercadoPagoConfig({
-  accessToken: process.env.MP_ACCESS_TOKEN!,
-})
-
-export const mpPreference = new Preference(client)
-export const mpPayment    = new Payment(client)
